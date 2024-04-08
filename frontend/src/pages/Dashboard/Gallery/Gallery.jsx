add0cb9d0ade17e4e01afd0592c44ae529058a17
manda <manda.li8686@gmail.com>
2024-04-08 12:08:39 -0500

    updated dashboard with delete song

--- frontend/src/pages/Dashboard/Gallery/Gallery.jsx
+++ frontend/src/pages/Dashboard/Gallery/Gallery.jsx
@@ -1,12 +1,62 @@
 import "./Gallery.css";
 import VideoPlayer from "./VideoPlayer/VideoPlayer";
+import React, { useState } from 'react';
 
 const Gallery = ({ song }) => {
+  //const [selectedModel, setSelectedModel] = useState('');
   const generatedVideos = [1, 2, 3];
+  const modelTitles = ["Mapdreamer", "Drawing", "Faces"]; 
+  const [checkedModels, setCheckedModels] = useState({
+    Mapdreamer: false,
+    Drawing: false,
+    Faces: false
+  });
+
+  // Handle change for checkboxes
+  const handleCheckboxChange = (model) => {
+    setCheckedModels((prevCheckedModels) => ({
+      ...prevCheckedModels,
+      [model]: !prevCheckedModels[model]
+    }));
+  };
+
+  const renderTable = () => {
+    return (
+      <table className="generated-table">
+        <tbody>
+          {modelTitles.map((model, index) => (
+            
+            <React.Fragment key={index}>
+              <tr>
+                <td className="gif-placeholder">GIF placeholder for {model}</td>
+              </tr>
+              <tr>
+                <td>
+                  <label>
+                    <input
+                      type="checkbox"
+                      name={model}
+                      checked={checkedModels[model]}
+                      onChange={() => handleCheckboxChange(model)}
+                    />
+                    {model}
+                  </label>
+                </td>
+              </tr>
+              <button className='large-button'>Select Model and Generate</button>
+              
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
 
   return (
     <div id="gallery">
       <h1>{song}</h1>
+      
+      {song && renderTable()}
       {generatedVideos.map((video, index) => (
         <VideoPlayer video={video} key={index} />
       ))}
--- frontend/src/pages/Dashboard/Navbar/SongList/SongList.jsx
+++ frontend/src/pages/Dashboard/Navbar/SongList/SongList.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { getUserSongs } from "../../../../api/song"; // Adjust the import path according to your file structure
 import "./SongList.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const SongList = ({ onSongSelect }) => {
   const [songs, setSongs] = useState([]);
@@ -12,15 +14,27 @@ const SongList = ({ onSongSelect }) => {
     });
   }, []);
 
+  const handleDelete = (event, songTitle) => {
+    event.stopPropagation(); 
+    const isConfirmed = window.confirm(`Delete song ${songTitle}?`);
+    if (isConfirmed) {
+      const updatedSongs = songs.filter(song => song.title !== songTitle);
+      setSongs(updatedSongs);
+      localStorage.setItem('songs', JSON.stringify(updatedSongs));
+      alert(`Song "${songTitle}" deleted.`);
+    }
+  };
+
   return (
     <div className="song-list">
       {songs.map((song, index) => (
-        <div
-          key={index}
-          className="song-item"
-          onClick={() => onSongSelect(song.title)}
-        >
-          {song.title}
+        <div key={index} className="song-item">
+          <span onClick={() => onSongSelect(song.title)}>
+            {song.title}
+          </span>
+          <span className="delete-icon" onClick={(event) => handleDelete(event, song.title)}>
+          <FontAwesomeIcon icon={faTimes} />
+          </span>
         </div>
       ))}
     </div>
