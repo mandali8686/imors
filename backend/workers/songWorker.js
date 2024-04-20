578265f6a88f5c443bd9a93629eec8d27be125ee
yigitduzenlioglu <yigit.duzenlioglu@vanderbilt.edu>
2024-04-20 18:55:00 -0500

    worker logic edit

--- backend/workers/songWorker.js
+++ backend/workers/songWorker.js
@@ -7,18 +7,76 @@ const { sendEmail } = require("../utils/mailer");
 
 const User = require("../models/user");
 
+// Function to download file using HTTPS and save locally
+function downloadFile(url, destPath) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(destPath);
+
+    https
+      .get(url, (response) => {
+        if (response.statusCode !== 200) {
+          file.close();
+          fs.unlink(destPath, () => {}); // Delete the file async. (No need to wait)
+          reject(
+            `Server responded with ${response.statusCode}: ${response.statusMessage}`
+          );
+          return;
+        }
+
+        response.pipe(file);
+
+        file.on("finish", () => {
+          file.close();
+          resolve();
+        });
+      })
+      .on("error", (err) => {
+        file.close();
+        fs.unlink(destPath, () => {}); // Delete the file async. (No need to wait)
+        reject(err.message);
+      });
+
+    file.on("error", (err) => {
+      // Handle errors on file write
+      file.close();
+      fs.unlink(destPath, () => {}); // Delete the file async. (No need to wait)
+      reject(err.message);
+    });
+  });
+}
+
 const songWorker = new Worker(
   "songProcessing",
   async (job) => {
-    const {
-      filename,
-      filePath,
-      modelName,
-      userId,
-      songId,
-      videoId,
-      userEmail,
-    } = job.data;
+    const { filename, modelName, userId, song, videoId, userEmail } = job.data;
+
+    const fileRef = bucket.file(`${song.owner}/${song._id}/${song._id}`);
+    console.log("***************FILEREF:", fileRef);
+    const [url] = await fileRef.getSignedUrl({
+      action: "read",
+      expires: "03-09-2491",
+    });
+    console.log("Download URL obtained:", url);
+
+    const videoDirectory = path.join(
+      "C:",
+      "Users",
+      "yduze",
+      "songs",
+      song.owner,
+      song._id,
+      "videos"
+    );
+    console.log("Video directory path:", videoDirectory);
+    await fs.promises.mkdir(videoDirectory, { recursive: true });
+    console.log("Video directory ensured.");
+
+    const audioFilePath = path.join(videoDirectory, song._id + ".mp3");
+    console.log("Audio file path:", audioFilePath);
+
+    await downloadFile(url, audioFilePath);
+    console.log("Audio file downloaded and saved successfully.");
+
     const outputFilePath = path.join(__dirname, "output", `${filename}.mp4`);
     const stylePath = path.join("C:\\lucid-sonic-dreams", `${modelName}.pkl`);
     const scriptPath = path.join(__dirname, "script.py");
@@ -30,11 +88,11 @@ const songWorker = new Worker(
     console.log(`Python Path: ${anacondaEnvPath}`);
     console.log(`Script Path: ${scriptPath}`);
     console.log(`Model Path: ${stylePath}`);
-    console.log(`File Path: ${filePath}`);
+    console.log(`File Path: ${audioFilePath}`);
 
     // Use spawn to run the Python script using the Python executable from the Anaconda environment
     const command = anacondaEnvPath;
-    const args = [scriptPath, filePath, outputFilePath, stylePath];
+    const args = [scriptPath, audioFilePath, outputFilePath, stylePath];
 
     const process = spawn(command, args, { shell: true });
 
